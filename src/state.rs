62e1342721e4bc8364ad27c63f23cf40d8d86cdf
Steven Chang <stevenchang5000@gmail.com>
2021-11-24 09:09:05 +1100

    More deprecation

--- src/state.rs
+++ src/state.rs
@@ -20,6 +20,26 @@ pub struct Constants {
     pub contract_address: HumanAddr,
 }
 
+pub struct ReadonlyConfig<'a, S: ReadonlyStorage> {
+    storage: ReadonlyPrefixedStorage<'a, S>,
+}
+
+impl<'a, S: ReadonlyStorage> ReadonlyConfig<'a, S> {
+    pub fn from_storage(storage: &'a S) -> Self {
+        Self {
+            storage: ReadonlyPrefixedStorage::new(PREFIX_CONFIG, storage),
+        }
+    }
+
+    fn as_readonly(&self) -> ReadonlyConfigImpl<ReadonlyPrefixedStorage<S>> {
+        ReadonlyConfigImpl(&self.storage)
+    }
+
+    pub fn constants(&self) -> StdResult<Constants> {
+        self.as_readonly().constants()
+    }
+}
+
 fn ser_bin_data<T: Serialize>(obj: &T) -> StdResult<Vec<u8>> {
     bincode2::serialize(&obj).map_err(|e| StdError::serialize_err(type_name::<T>(), e))
 }
@@ -53,4 +73,46 @@ pub fn write_viewing_key<S: Storage>(store: &mut S, owner: &CanonicalAddr, key: 
 pub fn read_viewing_key<S: Storage>(store: &S, owner: &CanonicalAddr) -> Option<Vec<u8>> {
     let balance_store = ReadonlyPrefixedStorage::new(PREFIX_VIEW_KEY, store);
     balance_store.get(owner.as_slice())
+}
+
+pub struct Config<'a, S: Storage> {
+    storage: PrefixedStorage<'a, S>,
+}
+
+impl<'a, S: Storage> Config<'a, S> {
+    pub fn from_storage(storage: &'a mut S) -> Self {
+        Self {
+            storage: PrefixedStorage::new(PREFIX_CONFIG, storage),
+        }
+    }
+
+    fn as_readonly(&self) -> ReadonlyConfigImpl<PrefixedStorage<S>> {
+        ReadonlyConfigImpl(&self.storage)
+    }
+
+    pub fn constants(&self) -> StdResult<Constants> {
+        self.as_readonly().constants()
+    }
+
+    pub fn set_constants(&mut self, constants: &Constants) -> StdResult<()> {
+        set_bin_data(&mut self.storage, KEY_CONSTANTS, constants)
+    }
+}
+
+/// This struct refactors out the readonly methods that we need for `Config` and `ReadonlyConfig`
+/// in a way that is generic over their mutability.
+///
+/// This was the only way to prevent code duplication of these methods because of the way
+/// that `ReadonlyPrefixedStorage` and `PrefixedStorage` are implemented in `cosmwasm-std`
+struct ReadonlyConfigImpl<'a, S: ReadonlyStorage>(&'a S);
+
+impl<'a, S: ReadonlyStorage> ReadonlyConfigImpl<'a, S> {
+    fn constants(&self) -> StdResult<Constants> {
+        let consts_bytes = self
+            .0
+            .get(KEY_CONSTANTS)
+            .ok_or_else(|| StdError::generic_err("no constants stored in configuration"))?;
+        bincode2::deserialize::<Constants>(&consts_bytes)
+            .map_err(|e| StdError::serialize_err(type_name::<Constants>(), e))
+    }
 }
